8b269ad096aed6b3ade017ad8775d8fdeb4a2fa8
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-07-24 15:40:31 +0000

    Add footer component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router';
 import NavigationBar from './Navbar';
+import { FooterWithLogo } from './Footer';
 
 function Layout() {
   return (
     <>
       <NavigationBar />
-      <Outlet />
+        <Outlet />
+      <FooterWithLogo />
     </>
   );
 }
