903476cc5278a45b7ba27874da03ef9a8af79084
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-07-26 09:44:38 +0000

    Add registration form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,27 @@
+import { Button, Card, CardBody, CardHeader, Input, Tab, TabPanel, Tabs, TabsBody, TabsHeader } from "@material-tailwind/react"
 
 function Login() {
   return (
-    <section className="min-h-screen">
-      <p>Login Here</p>
+    <section className="min-h-screen flex justify-center">
+      <Card className="w-full mx-20 my-10">
+        <CardHeader color="blue-gray" className="m-0 grid place-items-center rounded-b-none py-8 px-4 text-center">Register here</CardHeader>
+        <CardBody>
+          <Tabs className="overflow-visible" value="buy">
+            <TabsHeader>
+              <Tab value="buy">Buy Voucher</Tab>
+              <Tab value="data">Bio Data</Tab>
+              <Tab value="confirm">Confirm</Tab>
+              <TabsBody>
+                <TabPanel value="buy" className="flex flex-col ">
+                  <Input label="Name"></Input>
+                  <Button color="green" className="capitalize">Buy voucher</Button>
+                </TabPanel>
+
+              </TabsBody>
+            </TabsHeader>
+          </Tabs>
+        </CardBody>
+      </Card>
     </section>
   )
 }
